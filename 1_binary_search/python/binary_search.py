02ecd26d4205f43f917bfc5fcfc70ae445782a07
z14633 <zhangzhijun18@outlook.com>
2020-06-17 21:08:31 +0800

    内建函数——len

--- 1_binary_search/python/binary_search.py
+++ 1_binary_search/python/binary_search.py
@@ -3,6 +3,7 @@
 def binary_search(list, item):
     low = 0
     high = len(list) - 1
+    #内建函数这里记不清了，一时想不起来python要如何才能计算出数组的长度
     while low <= high:
         mid = (low + high) // 2
         if list[mid] == item:
