6f5cde24620da547484b8e3caf4cc171fb7f406d
z14633 <zhangzhijun18@outlook.com>
2020-06-17 21:03:41 +0800

    python 二分查找法

---
+++ 1_binary_search/python/binary_search.py
@@ -0,0 +1,20 @@
+# 第一个算法——二分查找法：思想：在一个有序的数组中，先找中间的
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if list[mid] == item:
+            return mid
+        if list[mid] > item:
+            high = mid - 1
+        if list[mid] < item:
+            low = mid + 1
+
+
+arr = [1, 2, 4, 6, 9]
+
+print(binary_search(arr, 6))
+
+print(binary_search(arr, -1))
